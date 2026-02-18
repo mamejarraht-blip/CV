62a2558600772bb6cda15bf0616ee7ccf518af26
mamejarraht-blip <mamejarraht@gmail.com>
2026-02-18 17:39:41 +0000

    Add files via upload

---
+++ cv.py
@@ -0,0 +1,20 @@
+import streamlit as st
+st.subheader("Technicienne Géomatique")
+
+st.markdown("""je suis technicienne en géomatique,je maitrise les outils des Systémes d'Iformation Géographique SIG""")
+
+st.header("formation")
+col1,=st.columns(1)
+with col1:
+    st.write("""Diplome de technicienne Superieur en /CEDT Le G15""")
+
+
+st.header("compétences")
+col1=st.columns(1)
+with col1:
+    st.subheader("Géeomatique")
+    st.write("* Connaissance et traitement des données sur Arcmap et Qgis")
+    st.write("* Maitrise des outils SIG Systémes d'Informaton Géographique")
+    st.write("* Etude topographique")
+    st.write("* Réalisation de projet batiment 2D sue Autocad")
+    st.write("* Réalisation de projet batiment 3D SUR Sketchup")
